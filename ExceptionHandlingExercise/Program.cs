1af98fdc44dcb4bfdcca1e7b71d3222f0f4ea44e
jose <jrodriguez@itsolutionspcllc.com>
2022-11-26 23:13:33 -0500

    ExceptionHandlingExercise

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -9,11 +9,31 @@ namespace ExceptionHandlingExercise
         static void Main(string[] args)
         {
             // -----------------------------------------------------------------------------
-            // First create an char[], it must contain 6 numbers and 3 letters - name it arr
-            // Create a list called numbers that will hold integers
-            // Create an string variable with an empty string initializer - name it str
+            // done First create an char[], it must contain 6 numbers and 3 letters - name it arr
+            var arr = new char[9];
 
-            // using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
+            arr[0] = 'a';
+            arr[1] = 'b';
+            arr[2] = 'c';
+            arr[3] = '1';
+            arr[4] = '2';
+            arr[5] = '3';
+            arr[6] = '4';
+            arr[7] = '5';
+            arr[8] = '6';
+
+
+
+
+            // done Create a list called numbers that will hold integers
+
+              var numbers = new List<int>();
+
+            //done  Create an string variable with an empty string initializer - name it str
+               string str = "";
+            //done  using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
+
+
             // and Exceptions will be thrown 
             // Below we will set this up 
             // ------------------------------------------------------------------------------
@@ -21,28 +41,53 @@ namespace ExceptionHandlingExercise
 
 
             //TODO START HERE:
-            
-            // Make a foreach loop to iterate through your character array
-            
-                // Now create a try catch
-                
-                
-                    // Inside your try block
-                        // set your string variable to each array element in your char[] to .ToString()
-                        // Now, using int.Parse, parse your string variable and store in an int variable
-                        // Then add each int to your list
-                
-                // catch your Exception:
-                // in the scope of your catch you can use the following, 
-                
-                    //Console.WriteLine($"Unable to Parse '{character}'"); //character will be the name of each item in your collection
-                
-            
 
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+            // done Make a foreach loop to iterate through your character array
+
+            foreach (var item in arr)
+            {
+                // Now create a try catch
+
+
+                // Inside your try block
+                // set your string variable to each array element in your char[] to .ToString()
+                // Now, using int.Parse, parse your string variable and store in an int variable
+                // Then add each int to your list
+
+                try
+                {
+                    str = item.ToString();
+                    var number = int.Parse(str);
+                    numbers.Add(number);
+
+
+                }
+                catch (Exception e)
+
+                {
+                    Console.WriteLine($"Unable to Parse '{item}'");
+                }
+                finally 
+                { 
+                
+                 Console.WriteLine(item);
+                
+                }
+
+            }
+
+           
+            // catch your Exception:
+            // in the scope of your catch you can use the following, 
+
+            //Console.WriteLine($"Unable to Parse '{character}'"); //character will be the name of each item in your collection
+
+
+
+            foreach (var num in numbers)
+            {
+               Console.WriteLine(num);
+            }
         }
     }
 }
